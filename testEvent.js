b02503d4815a437048b073d9c0bc3534700f8f64
Arik Cohen <arikc@microsoft.com>
2019-01-22 09:28:56 -0800

    convert playerid to entityid

--- testEvent.js
+++ testEvent.js
@@ -1,11 +1,21 @@
 var entity;
+var server;
 handlers["sendTestEventPlayer"] = function (args, context) {
     // write event
     log.info("currentEntity", context.currentEntity);
     var entityEvent = {};
-    entityEvent.Entity = { Id: context.currentEntity.Entity, Type: "title_player_account" };
     entityEvent.EventNamespace = "com.playfab.events.triplify";
     entityEvent.Name = "test_event_player";
+    if (context.currentEntity == null) {
+        //do accountInfo to look up title_player_account
+        var accountInfo = server.GetUserAccountInfo({ PlayFabId: currentPlayerId });
+        //Get the Title Player Account
+        var titlePlayerAccount = accountInfo.UserInfo.TitleInfo.TitlePlayerAccount;
+        entityEvent.Entity = { Id: titlePlayerAccount, Type: "title_player_account" };
+    }
+    else {
+        entityEvent.Entity = { Id: context.currentEntity.Entity, Type: "title_player_account" };
+    }
     var eventResult = entity.WriteEvents({ Events: [entityEvent] });
     log.info("Write Events Result", eventResult);
 };
--- testEvent.ts
+++ testEvent.ts
@@ -1,14 +1,30 @@
 var entity: any;
+var server: any;
 
 handlers["sendTestEventPlayer"]  = function (args: any, context: IPlayFabContext): void {  
     
     // write event
     log.info("currentEntity", context.currentEntity);
 
+
     var entityEvent:any = {};
-    entityEvent.Entity = {Id: context.currentEntity.Entity, Type: "title_player_account"};
     entityEvent.EventNamespace = "com.playfab.events.triplify";
     entityEvent.Name = "test_event_player";    
+
+
+    if (context.currentEntity == null)
+    {
+        //do accountInfo to look up title_player_account
+        var accountInfo = server.GetUserAccountInfo({PlayFabId:currentPlayerId});
+  
+        //Get the Title Player Account
+        var titlePlayerAccount = accountInfo.UserInfo.TitleInfo.TitlePlayerAccount;
+        entityEvent.Entity = {Id: titlePlayerAccount, Type: "title_player_account"};
+    }
+    else {
+        entityEvent.Entity = {Id: context.currentEntity.Entity, Type: "title_player_account"};
+    }
+        
     var eventResult = entity.WriteEvents({Events:[entityEvent]});
     log.info("Write Events Result", eventResult);
     
